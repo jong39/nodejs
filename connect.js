50729b086d477895924e9817efd55d680eb32a5b
Jongpil Choi <jongpil.choi@gmail.com>
2018-07-18 17:49:32 -0700

    7/18 update again

---
+++ connect.js
@@ -0,0 +1,21 @@
+var connect = require('connect');
+var http = require('http');
+
+var app = connect();
+
+function doFirst(request, response, next){
+	console.log("Bacon");
+}
+
+function profile(request, response){
+	console.log("Profile page.");
+}
+
+//app.use(doFirst);
+app.use('/profile', profile);
+
+http.createServer(app).listen(8888);
+console.log("Server is running...");
+
+
+ 
